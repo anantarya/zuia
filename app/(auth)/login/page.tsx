07074613280980e60b4e83140dbeac0ca05f5d48
anantarya <144280517+anantarya@users.noreply.github.com>
2024-04-27 15:24:09 +0530

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/auth/LoginForm";
 import React from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authoptions";
 
 const page = async () => {
   const session = await getServerSession(authOptions);
